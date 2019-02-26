33fc30d792a40bbc36790aff0c9faf3b4862d987
mephody-bro <kirill.fedyanin@gmail.com>
2019-02-26 11:43:40 +0300

    Remove duplicates

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,8 @@
-from os import listdir
+import os
 from PIL import Image
+from PIL import ImageChops
 import PIL
+import numpy as np
 
 TARGET_SIZE = (236, 236)
 DATA_DIR = 'data/'
@@ -12,14 +14,29 @@ def preprocess():
 
 def remove_duplicates():
     """Remove duplicates based on euristic rule"""
+    prev_image = None
+    for image_name in reversed(sorted(os.listdir(DATA_DIR))):
+        image = Image.open(DATA_DIR + image_name)
+
+        if prev_image is None:
+            prev_image = image
+            continue
+
+        if image.size == prev_image.size:
+            diff = np.array(image) - np.array(prev_image)
+            if not np.any(diff):
+                os.remove(DATA_DIR + image_name)
+                print(image_name, 'deleted')
+
+        prev_image = image
 
 
 def show_dimensions():
-    """Print dimensions of biggest and smallest images"""
+    """Print biggest and smallest image dimensions"""
     min_d = (10e6, 10e6)
     max_d = (0, 0)
 
-    for image_name in listdir(DATA_DIR):
+    for image_name in os.listdir(DATA_DIR):
         image = Image.open(DATA_DIR + image_name)
         height, width = image.size
 
@@ -33,4 +50,4 @@ def show_dimensions():
 
 
 if __name__ == "__main__":
-    show_dimensions()
+    remove_duplicates()
